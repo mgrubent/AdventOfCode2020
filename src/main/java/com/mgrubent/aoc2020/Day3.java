55181e55065d2670136c43084c93e08c061c5d8b
Michael Gruben Trejo <mgrubent@linkedin.com>
2020-12-06 19:11:28 -0800

    Add stubs for Day3

---
+++ src/main/java/com/mgrubent/aoc2020/Day3.java
@@ -0,0 +1,27 @@
+package com.mgrubent.aoc2020;
+
+public class Day3 extends Puzzle {
+    /**
+     * Constructor which accepts the puzzle input to be solved
+     *
+     * @param input the puzzle input
+     */
+    Day3(String input) {
+        super(input);
+    }
+
+    @Override
+    int getDay() {
+        return 3;
+    }
+
+    @Override
+    String solve1() {
+        return null;
+    }
+
+    @Override
+    String solve2() {
+        return null;
+    }
+}
---
+++ src/test/java/com/mgrubent/aoc2020/Day3Test.java
@@ -0,0 +1,4 @@
+package com.mgrubent.aoc2020;
+
+public class Day3Test extends BasePuzzleTest {
+}
