33094a061e17a4cac6707bb5f1c20ea5c5c74272
Michael Gruben Trejo <mgrubent@linkedin.com>
2020-12-06 19:15:49 -0800

    Make testing more general?

--- src/test/java/com/mgrubent/aoc2020/BasePuzzleTest.java
+++ src/test/java/com/mgrubent/aoc2020/BasePuzzleTest.java
@@ -1,14 +1,27 @@
 package com.mgrubent.aoc2020;
 
 import com.google.common.io.Resources;
+import org.junit.Test;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class BasePuzzleTest {
+public abstract class BasePuzzleTest {
 
     public String getStoredInput(int day) throws Exception {
         var puzzleInputResource = Resources.getResource("puzzle/" + day);
         return Files.readString(Path.of(puzzleInputResource.toURI()));
     }
+
+    @Test
+    abstract public void testExample1();
+
+    @Test
+    abstract public void testPart1() throws Exception;
+
+    @Test
+    abstract public void testExample2();
+
+    @Test
+    abstract public void testPart2() throws Exception;
 }
--- src/test/java/com/mgrubent/aoc2020/Day3Test.java
+++ src/test/java/com/mgrubent/aoc2020/Day3Test.java
@@ -1,4 +1,23 @@
 package com.mgrubent.aoc2020;
 
 public class Day3Test extends BasePuzzleTest {
+    @Override
+    public void testExample1() {
+
+    }
+
+    @Override
+    public void testPart1() {
+        
+    }
+
+    @Override
+    public void testExample2() {
+
+    }
+
+    @Override
+    public void testPart2() {
+
+    }
 }
