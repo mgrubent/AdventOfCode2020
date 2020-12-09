3948024b3414d0b49ea8e64c0c4083b8c0ae8b73
Michael Gruben Trejo <mgrubent@linkedin.com>
2020-12-08 19:53:31 -0800

    Solve part 2

--- src/main/java/com/mgrubent/aoc2020/Day4.java
+++ src/main/java/com/mgrubent/aoc2020/Day4.java
@@ -1,17 +1,10 @@
 package com.mgrubent.aoc2020;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day4 extends Puzzle {
-    private static final String BIRTH_YEAR = "byr";
-    private static final String ISSUE_YEAR = "iyr";
-    private static final String EXPIRATION_YEAR = "eyr";
-    private static final String HEIGHT = "hgt";
-    private static final String HAIR_COLOR = "hcl";
-    private static final String EYE_COLOR = "ecl";
-    private static final String PASSPORT_ID = "pid";
-    private static final String COUNTRY_ID = "cid";
-
     private final List<Passport> _passports = new LinkedList<>();
 
     /**
@@ -47,16 +40,26 @@ public class Day4 extends Puzzle {
 
     @Override
     String solve1() {
-        return Long.toString(_passports.stream().filter(Passport::isValid).count());
+        return Long.toString(_passports.stream().filter(Passport::isValid1).count());
     }
 
     @Override
     String solve2() {
-        return null;
+        return Long.toString(_passports.stream().filter(Passport::isValid2).count());
     }
 }
 
 class Passport {
+    private static final String BIRTH_YEAR = "byr";
+    private static final String ISSUE_YEAR = "iyr";
+    private static final String EXPIRATION_YEAR = "eyr";
+    private static final String HEIGHT = "hgt";
+    private static final String HAIR_COLOR = "hcl";
+    private static final String EYE_COLOR = "ecl";
+    private static final String PASSPORT_ID = "pid";
+    private static final String COUNTRY_ID = "cid";
+
+
     private static final Set<String> REQUIRED_KEYS = Set.of("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid");
     private static final Set<String> VALID_KEYS = Set.of("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid");
 
@@ -81,10 +84,106 @@ class Passport {
                 '}';
     }
 
-    boolean isValid() {
+    boolean isValid1() {
         boolean allKeysValid = VALID_KEYS.containsAll(_properties.keySet());
         boolean hasAllRequiredKeys = _properties.keySet().containsAll(REQUIRED_KEYS);
         return hasAllRequiredKeys && allKeysValid;
+    }
+
+    private boolean byrValid() {
+        try {
+            int byr = Integer.parseInt(_properties.get(BIRTH_YEAR));
+            return byr >= 1920 && byr <= 2002;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+
+    }
+
+    private boolean iyrValid() {
+        try {
+            int iyr = Integer.parseInt(_properties.get(ISSUE_YEAR));
+            return iyr >= 2010 && iyr <= 2020;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+    }
+
+    private boolean eyrValid() {
+        try {
+            int eyr = Integer.parseInt(_properties.get(EXPIRATION_YEAR));
+            return eyr >= 2020 && eyr <= 2030;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+    }
+
+    private boolean hgtCmValid(int value) {
+        return value >= 150 && value <= 193;
+    }
+
+    private boolean hgtInValid(int value) {
+        return value >= 59 && value <= 76;
+    }
+
+    private boolean hgtValid() {
+        String hgtString = _properties.get(HEIGHT);
+
+        Pattern heightRe = Pattern.compile("(?<value>[0-9]+)(?<unit>in|cm)");
+        Matcher m = heightRe.matcher(hgtString);
+        if (m.find()) {
+            int value;
+            try {
+                value = Integer.parseInt(m.group("value"));
+            } catch (NumberFormatException nfe) {
+                return false;
+            }
+
+            switch (m.group("unit")) {
+                case "in" -> {
+                    return hgtInValid(value);
+                }
+                case "cm" -> {
+                    return hgtCmValid(value);
+                }
+
+            }
+        }
+        return false;
+
+    }
+
+    private boolean hclValid() {
+        Pattern hairColorRe = Pattern.compile("#[0-9a-f]{6}");
+
+        String hclString = _properties.get(HAIR_COLOR);
+
+        Matcher m = hairColorRe.matcher(hclString);
+        return m.matches();
+    }
+
+    private boolean eclValid() {
+        Set<String> validEyeColors = Set.of("amb", "blu", "brn", "gry", "grn", "hzl", "oth");
+        String eclString = _properties.get(EYE_COLOR);
+
+        return validEyeColors.contains(eclString);
+    }
+
+    private boolean pidValid() {
+        Pattern passportRe = Pattern.compile("[0-9]{9}");
+
+        String pidString = _properties.get(PASSPORT_ID);
+
+        Matcher m = passportRe.matcher(pidString);
+        return m.matches();
+    }
+
+    private boolean strictRequirementsMet() {
+        return byrValid() && iyrValid() && eyrValid() && hgtValid() && hclValid() && eclValid() && pidValid();
+    }
+
+    boolean isValid2() {
+        return isValid1() && strictRequirementsMet();
     }
 
 }
--- src/test/java/com/mgrubent/aoc2020/Day4Test.java
+++ src/test/java/com/mgrubent/aoc2020/Day4Test.java
@@ -17,6 +17,7 @@ public class Day4Test extends BasePuzzleTest {
 
             hcl:#cfa07d eyr:2025 pid:166559648
             iyr:2011 ecl:brn hgt:59in""";
+
     @Override
     public void testExample1() {
         var day4 = new Day4(EXAMPLE);
@@ -25,16 +26,52 @@ public class Day4Test extends BasePuzzleTest {
 
     @Override
     public void testPart1() throws Exception {
-        Assert.assertFalse(true);
+        var day4 = new Day4(getStoredInput(4));
+        Assert.assertEquals("204", day4.solve1());
+
     }
 
     @Override
     public void testExample2() {
-        Assert.assertFalse(true);
+        String invalidPassports = """
+                eyr:1972 cid:100
+                hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+                                
+                iyr:2019
+                hcl:#602927 eyr:1967 hgt:170cm
+                ecl:grn pid:012533040 byr:1946
+                                
+                hcl:dab227 iyr:2012
+                ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+                                
+                hgt:59cm ecl:zzz
+                eyr:2038 hcl:74454a iyr:2023
+                pid:3556412378 byr:2007""";
+
+        var day4 = new Day4(invalidPassports);
+        Assert.assertEquals("0", day4.solve2());
+
+        String validPassports = """
+                pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+                hcl:#623a2f
+                                
+                eyr:2029 ecl:blu cid:129 byr:1989
+                iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+                                
+                hcl:#888785
+                hgt:164cm byr:2001 iyr:2015 cid:88
+                pid:545766238 ecl:hzl
+                eyr:2022
+                                
+                iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719""";
+
+        day4 = new Day4(validPassports);
+        Assert.assertEquals("4", day4.solve2());
     }
 
     @Override
     public void testPart2() throws Exception {
-        Assert.assertFalse(true);
+        var day4 = new Day4(getStoredInput(4));
+        Assert.assertTrue(180 > Integer.parseInt(day4.solve2()));
     }
 }
