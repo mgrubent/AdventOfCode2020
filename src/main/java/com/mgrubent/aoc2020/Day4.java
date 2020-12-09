295deece4b8748b03a9161e7b8f8cbca889413c6
Michael Gruben Trejo <mgrubent@linkedin.com>
2020-12-08 19:21:40 -0800

    Solve part 1

--- src/main/java/com/mgrubent/aoc2020/Day4.java
+++ src/main/java/com/mgrubent/aoc2020/Day4.java
@@ -1,6 +1,19 @@
 package com.mgrubent.aoc2020;
 
+import java.util.*;
+
 public class Day4 extends Puzzle {
+    private static final String BIRTH_YEAR = "byr";
+    private static final String ISSUE_YEAR = "iyr";
+    private static final String EXPIRATION_YEAR = "eyr";
+    private static final String HEIGHT = "hgt";
+    private static final String HAIR_COLOR = "hcl";
+    private static final String EYE_COLOR = "ecl";
+    private static final String PASSPORT_ID = "pid";
+    private static final String COUNTRY_ID = "cid";
+
+    private final List<Passport> _passports = new LinkedList<>();
+
     /**
      * Constructor which accepts the puzzle input to be solved
      *
@@ -8,6 +21,23 @@ public class Day4 extends Puzzle {
      */
     Day4(String input) {
         super(input);
+
+        // Hackily parse and tokenize the input
+        Map<String, String> tmpMap = new HashMap<>();
+        String[] keyAndValue;
+        for (String line : input.split("\\n")) {
+            if (line.isEmpty()) {
+                _passports.add(Passport.fromMap(tmpMap));
+                tmpMap.clear();
+            } else {
+                for (String token : line.split(" ")) {
+                    keyAndValue = token.split(":");
+                    tmpMap.put(keyAndValue[0], keyAndValue[1]);
+                }
+            }
+        }
+        _passports.add(Passport.fromMap(tmpMap));
+        tmpMap.clear();
     }
 
     @Override
@@ -17,11 +47,44 @@ public class Day4 extends Puzzle {
 
     @Override
     String solve1() {
-        return null;
+        return Long.toString(_passports.stream().filter(Passport::isValid).count());
     }
 
     @Override
     String solve2() {
         return null;
     }
+}
+
+class Passport {
+    private static final Set<String> REQUIRED_KEYS = Set.of("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid");
+    private static final Set<String> VALID_KEYS = Set.of("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid");
+
+
+    static Passport fromMap(Map<String, String> map) {
+        Passport passport = new Passport();
+        map.forEach(passport::put);
+        return passport;
+    }
+
+    private Map<String, String> _properties = new HashMap<>();
+
+    Passport put(String key, String value) {
+        _properties.put(key, value);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Passport{" +
+                "_properties=" + _properties +
+                '}';
+    }
+
+    boolean isValid() {
+        boolean allKeysValid = VALID_KEYS.containsAll(_properties.keySet());
+        boolean hasAllRequiredKeys = _properties.keySet().containsAll(REQUIRED_KEYS);
+        return hasAllRequiredKeys && allKeysValid;
+    }
+
 }
--- src/test/java/com/mgrubent/aoc2020/Day4Test.java
+++ src/test/java/com/mgrubent/aoc2020/Day4Test.java
@@ -3,9 +3,24 @@ package com.mgrubent.aoc2020;
 import org.junit.Assert;
 
 public class Day4Test extends BasePuzzleTest {
+    private static final String EXAMPLE = """
+            ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+            byr:1937 iyr:2017 cid:147 hgt:183cm
+
+            iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+            hcl:#cfa07d byr:1929
+
+            hcl:#ae17e1 iyr:2013
+            eyr:2024
+            ecl:brn pid:760753108 byr:1931
+            hgt:179cm
+
+            hcl:#cfa07d eyr:2025 pid:166559648
+            iyr:2011 ecl:brn hgt:59in""";
     @Override
     public void testExample1() {
-        Assert.assertFalse(true);
+        var day4 = new Day4(EXAMPLE);
+        Assert.assertEquals("2", day4.solve1());
     }
 
     @Override
