6786f5532acb3b112f86d17f7f78553aa9f3ba0d
Michael Gruben Trejo <mgrubent@linkedin.com>
2020-12-08 18:17:28 -0800

    Add stubs

---
+++ src/test/java/com/mgrubent/aoc2020/Day4Test.java
@@ -0,0 +1,25 @@
+package com.mgrubent.aoc2020;
+
+import org.junit.Assert;
+
+public class Day4Test extends BasePuzzleTest {
+    @Override
+    public void testExample1() {
+        Assert.assertFalse(true);
+    }
+
+    @Override
+    public void testPart1() throws Exception {
+        Assert.assertFalse(true);
+    }
+
+    @Override
+    public void testExample2() {
+        Assert.assertFalse(true);
+    }
+
+    @Override
+    public void testPart2() throws Exception {
+        Assert.assertFalse(true);
+    }
+}
