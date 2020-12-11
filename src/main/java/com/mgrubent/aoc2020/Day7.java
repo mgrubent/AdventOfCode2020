81daeac9ea6b6d4f0b6a4257a52d846586747114
Michael Gruben Trejo <mgrubent@linkedin.com>
2020-12-11 14:04:05 -0800

    Add Day7 stub

---
+++ src/main/java/com/mgrubent/aoc2020/Day7.java
@@ -0,0 +1,27 @@
+package com.mgrubent.aoc2020;
+
+public class Day7 extends Puzzle {
+    /**
+     * Constructor which accepts the puzzle input to be solved
+     *
+     * @param input the puzzle input
+     */
+    Day7(String input) {
+        super(input);
+    }
+
+    @Override
+    int getDay() {
+        return 7;
+    }
+
+    @Override
+    String solve1() {
+        return null;
+    }
+
+    @Override
+    String solve2() {
+        return null;
+    }
+}
