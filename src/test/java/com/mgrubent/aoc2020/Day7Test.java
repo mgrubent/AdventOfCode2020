722fed21a619b5f2dd1c1e7700f5125f5908d40a
Michael Gruben Trejo <mgrubent@linkedin.com>
2020-12-11 14:04:41 -0800

    Add Day7Test stub

---
+++ src/test/java/com/mgrubent/aoc2020/Day7Test.java
@@ -0,0 +1,25 @@
+package com.mgrubent.aoc2020;
+
+import org.junit.Assert;
+
+public class Day7Test extends BasePuzzleTest {
+    @Override
+    public void testExample1() {
+        Assert.fail();
+    }
+
+    @Override
+    public void testPart1() throws Exception {
+        Assert.fail();
+    }
+
+    @Override
+    public void testExample2() {
+        Assert.fail();
+    }
+
+    @Override
+    public void testPart2() throws Exception {
+        Assert.fail();
+    }
+}
