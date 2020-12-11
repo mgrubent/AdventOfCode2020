578424699825f111817715221659434fe3c786a6
Michael Gruben Trejo <mgrubent@linkedin.com>
2020-12-11 15:49:14 -0800

    Add stubs

---
+++ src/main/java/com/mgrubent/aoc2020/Day8.java
@@ -0,0 +1,27 @@
+package com.mgrubent.aoc2020;
+
+public class Day8 extends Puzzle {
+    /**
+     * Constructor which accepts the puzzle input to be solved
+     *
+     * @param input the puzzle input
+     */
+    Day8(String input) {
+        super(input);
+    }
+
+    @Override
+    int getDay() {
+        return 8;
+    }
+
+    @Override
+    String solve1() {
+        return null;
+    }
+
+    @Override
+    String solve2() {
+        return null;
+    }
+}
