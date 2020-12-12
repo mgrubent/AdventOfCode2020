955837982c689bc9ad32c8caa3515dc1e40c054e
Michael Gruben Trejo <mgrubent@linkedin.com>
2020-12-12 11:41:44 -0800

    Add stubs

---
+++ src/main/java/com/mgrubent/aoc2020/Day9.java
@@ -0,0 +1,27 @@
+package com.mgrubent.aoc2020;
+
+public class Day9 extends Puzzle {
+    /**
+     * Constructor which accepts the puzzle input to be solved
+     *
+     * @param input the puzzle input
+     */
+    Day9(String input) {
+        super(input);
+    }
+
+    @Override
+    int getDay() {
+        return 9;
+    }
+
+    @Override
+    String solve1() {
+        return null;
+    }
+
+    @Override
+    String solve2() {
+        return null;
+    }
+}
---
+++ src/test/java/com/mgrubent/aoc2020/Day9Test.java
@@ -0,0 +1,23 @@
+package com.mgrubent.aoc2020;
+
+public class Day9Test extends BasePuzzleTest {
+    @Override
+    public void testExample1() {
+        
+    }
+
+    @Override
+    public void testPart1() throws Exception {
+
+    }
+
+    @Override
+    public void testExample2() {
+
+    }
+
+    @Override
+    public void testPart2() throws Exception {
+
+    }
+}
