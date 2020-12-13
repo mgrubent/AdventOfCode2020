eebf00ec414745e7b1404b4d18e81306f97ddfed
Michael Gruben Trejo <mgrubent@linkedin.com>
2020-12-13 12:20:01 -0800

    Pass all the given tests and examples

--- src/main/java/com/mgrubent/aoc2020/Day10.java
+++ src/main/java/com/mgrubent/aoc2020/Day10.java
@@ -1,6 +1,20 @@
 package com.mgrubent.aoc2020;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class Day10 extends Puzzle {
+    private final List<JoltageAdapter> _joltageAdapters;
+    private final Device _device;
+
+    // We expect only to have the differences 1, 2, and 3, and so this could fit in an array of size 3.
+    // That said, I really don't want to have to deal inline with the conversion from 1-indexed-counting
+    // to 0-indexed counting.  Accordingly, just accept the wasted space in exchange for greater readability.
+    private final int[] _joltageDifferences = new int[4];
+
     /**
      * Constructor which accepts the puzzle input to be solved
      *
@@ -8,6 +22,39 @@ public class Day10 extends Puzzle {
      */
     Day10(String input) {
         super(input);
+
+        // Collect all of the "joltages"
+        _joltageAdapters = input.lines().map(Integer::parseInt).map(JoltageAdapter::new).collect(Collectors.toList());
+
+        // Instantiate "my device" with the expected JoltageAdapter
+        _device = new Device(_joltageAdapters);
+
+
+        // Calculate difference distribution statistics when connecting the charging outlet near our seat (0 joltage)
+        // to our device, using all of our JoltageAdapters in series
+        List<Integer> sortedJoltageRatings = _joltageAdapters.stream()
+                .map(JoltageAdapter::getRating).sorted().collect(Collectors.toList());
+
+        // "Treat the charging outlet near your seat as having an effective joltage rating of 0."
+        sortedJoltageRatings.add(0, 0);
+
+        // Finally, we must consider the joltage rating of our device, which by definition is 3 higher
+        // than the highest joltage rating of any of our JoltageAdapters
+        sortedJoltageRatings.add(_device.getJoltageAdapter().getRating());
+
+        // Sum all the differences
+        for (int i = 0, j = 1; j < sortedJoltageRatings.size(); i++, j++) {
+            int joltageDifference = sortedJoltageRatings.get(j) - sortedJoltageRatings.get(i);
+            _joltageDifferences[joltageDifference]++;
+        }
+    }
+
+    int[] getJoltageDifferences() {
+        return Arrays.copyOf(_joltageDifferences, 4);
+    }
+
+    Device getDevice() {
+        return _device;
     }
 
     @Override
@@ -23,5 +70,42 @@ public class Day10 extends Puzzle {
     @Override
     String solve2() {
         return null;
+    }
+}
+
+class JoltageAdapter {
+    private final int _rating;
+
+    JoltageAdapter(int rating) {
+        _rating = rating;
+    }
+
+    int getRating() {
+        return _rating;
+    }
+
+    boolean canAcceptInputJoltage(int inputJoltage) {
+        // "Any given adapter can take an input 1, 2, or 3 jolts lower than its rating
+        // and still produce its rated output joltage."
+        return inputJoltage < _rating && inputJoltage >= _rating - 3;
+    }
+}
+
+class Device {
+    private final JoltageAdapter _joltageAdapter;
+
+    Device(JoltageAdapter joltageAdapter) {
+        _joltageAdapter = joltageAdapter;
+    }
+
+    Device(List<JoltageAdapter> joltageAdapters) {
+        Optional<JoltageAdapter> max = joltageAdapters.stream().max(Comparator.comparingInt(JoltageAdapter::getRating));
+        // "In addition, your device has a built-in joltage adapter rated for 3 jolts higher
+        // than the highest-rated adapter in your bag."
+        _joltageAdapter = new JoltageAdapter(max.get().getRating() + 3);
+    }
+
+    JoltageAdapter getJoltageAdapter() {
+        return _joltageAdapter;
     }
 }
--- src/test/java/com/mgrubent/aoc2020/Day10Test.java
+++ src/test/java/com/mgrubent/aoc2020/Day10Test.java
@@ -1,23 +1,105 @@
 package com.mgrubent.aoc2020;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Day10Test extends BasePuzzleTest {
+    private static final String SMALL_EXAMPLE = """
+            3
+            9
+            6""";
+
+    private static final String EXAMPLE = """
+            16
+            10
+            15
+            5
+            1
+            11
+            7
+            19
+            6
+            12
+            4""";
+
+    private static final String LARGER_EXAMPLE = """
+            28
+            33
+            18
+            42
+            31
+            14
+            46
+            20
+            48
+            47
+            24
+            23
+            49
+            45
+            19
+            38
+            39
+            11
+            1
+            32
+            25
+            35
+            8
+            17
+            7
+            9
+            4
+            2
+            34
+            10
+            3""";
+
+    @Test
+    public void testDevice() {
+        List<JoltageAdapter> joltageAdapters =
+                List.of(new JoltageAdapter(3), new JoltageAdapter(9), new JoltageAdapter(6));
+
+        JoltageAdapter expected = new JoltageAdapter(12);
+        JoltageAdapter actual = new Device(joltageAdapters).getJoltageAdapter();
+
+        Assert.assertEquals(expected.getRating(), actual.getRating());
+    }
+
     @Override
     public void testExample1() {
-        
+        var day10 = new Day10(SMALL_EXAMPLE);
+        Assert.assertEquals(12, day10.getDevice().getJoltageAdapter().getRating());
+
+        day10 = new Day10(EXAMPLE);
+        Assert.assertEquals(22, day10.getDevice().getJoltageAdapter().getRating());
+
+        // "In this example, when using every adapter, there are 7 differences of 1 jolt and 5 differences of 3 jolts"
+        int[] expected = new int[] {0, 7, 0, 5};
+        int[] actual = day10.getJoltageDifferences();
+        Assert.assertArrayEquals(expected, actual);
+
+        day10 = new Day10(LARGER_EXAMPLE);
+        expected = new int[] {0, 22, 0, 10};
+        actual = day10.getJoltageDifferences();
+        Assert.assertArrayEquals(expected, actual);
     }
 
     @Override
     public void testPart1() throws Exception {
-
+        Assert.fail();
     }
 
     @Override
     public void testExample2() {
-
+        Assert.fail();
     }
 
     @Override
     public void testPart2() throws Exception {
-
+        Assert.fail();
     }
 }
